bfd2294a4611d989bf7d6a379a8c16c4d981e820
Marcos Takao Barbosa <marcostkao@gmail.com>
2020-06-09 21:34:53 -0300

    Serviço HTTP para o Detalhe do Restaurante

--- src/app/restaurants/restaurant/restaurant.model.ts
+++ src/app/restaurants/restaurant/restaurant.model.ts
@@ -1,10 +1,9 @@
 export interface Restaurant {
-
-    id: string
-    name: string
-    category: string
-    deliveryEstimate: string
-    rating: number
+    id: string,
+    name: string,
+    category: string,
+    deliveryEstimate: string,
+    rating: number,
     imagePath: string
 
 }
