3f25769712158209b7708dac0c4b5a53bd08ace8
Marcos Takao Barbosa <marcostkao@gmail.com>
2020-06-16 21:42:37 -0300

    Criando Validadores Personalizados

--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms'
+import { FormGroup, FormBuilder, Validators, AbstractControl } from '@angular/forms'
 
 import { Router } from '@angular/router'
 
@@ -42,7 +42,20 @@ export class OrderComponent implements OnInit {
       optionalAddress: this.formBuilder.control(''),
       paymentOptions: this.formBuilder.control('', [Validators.required])
 
-    })
+    }, {validator: OrderComponent.equalsTo})
+  }
+
+  static equalsTo(group: AbstractControl): {[key:string]: boolean} {
+    const email = group.get('email')
+    const emailConfirmation = group.get('emailConfirmation')
+    if(!email || !emailConfirmation) {
+      return undefined
+    }
+
+    if(email.value !== emailConfirmation.value) {
+      return {emailsNotMatch:true}
+    }
+    return undefined
   }
 
   itemsValue(): number {
