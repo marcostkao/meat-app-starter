c6d900bb2a60fe23bc80cdc3bea4eff8742b6f35
Marcos Takao da Cunha Barbosa <MTBarbosa@rd.com.br>
2020-08-05 16:34:19 -0300

    Criando o Componente de Login

--- src/app/shared/input/input.component.ts
+++ src/app/shared/input/input.component.ts
@@ -10,6 +10,7 @@ export class InputComponent implements OnInit, AfterContentInit {
 
   @Input() label: string
   @Input() errorMessage: string
+  @Input() showTip: boolean = true
 
   input: any
 
@@ -23,7 +24,7 @@ export class InputComponent implements OnInit, AfterContentInit {
 
   ngAfterContentInit() {
     this.input = this.model || this.control
-    if(this.input === undefined){
+    if (this.input === undefined) {
       throw new Error('Esse componente precisa ser usado com uma diretiva ngModel ou formControlName')
     }
 
