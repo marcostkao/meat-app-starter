07334c90dba0489b087585aa0e2729b8f87ec74d
Marcos Takao da Cunha Barbosa <MTBarbosa@rd.com.br>
2020-08-05 21:58:42 -0300

    Realizando uma Compra com Autenticação

--- src/app/order/order.service.ts
+++ src/app/order/order.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from "@angular/core";
 
-import { HttpClient } from '@angular/common/http'
+import { HttpClient, HttpHeaders } from '@angular/common/http'
 import { Observable } from 'rxjs/Observable'
 import 'rxjs/add/operator/map'
 
@@ -9,20 +9,24 @@ import { CartItem } from 'app/restaurant-detail/shopping-cart/cart-item.model';
 import { Order } from './order.model'
 
 import { MEAT_API } from '../app.api'
+import { LoginService } from './../security/login/login.service';
+
 
 @Injectable()
 export class OrderService {
-  constructor(private cartService: ShoppingCartService, private http: HttpClient){}
+    constructor(private cartService: ShoppingCartService,
+        private http: HttpClient,
+        private loginService: LoginService) { }
 
     itemsValue(): number {
         return this.cartService.total()
     }
 
-    cartItems(): CartItem[]{
+    cartItems(): CartItem[] {
         return this.cartService.items
     }
 
-    increaseQty(item: CartItem){
+    increaseQty(item: CartItem) {
         this.cartService.increaseQty(item)
     }
 
@@ -30,7 +34,7 @@ export class OrderService {
         this.cartService.decreaseQty(item)
     }
 
-    remove(item: CartItem){
+    remove(item: CartItem) {
         this.cartService.removeItem(item)
     }
 
@@ -39,8 +43,12 @@ export class OrderService {
     }
 
     checkOrder(order: Order): Observable<string> {
-        return this.http.post<Order>(`${MEAT_API}/orders`, order)
-                            .map(order => order.id)
+        let headers = new HttpHeaders()
+        if (this.loginService.isLoggedIn()) {
+            headers = headers.set('Authorization', `Bearer ${this.loginService.user.accessToken}`)
+        }
+        return this.http.post<Order>(`${MEAT_API}/orders`, order, { headers: headers })
+            .map(order => order.id)
     }
 
 }
